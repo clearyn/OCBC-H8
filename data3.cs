fa3b97f1dff7eda044e4c58aa5abc807b0590234
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-10-12 13:27:57 +0700

    add sesi 4

---
+++ data3.cs
@@ -0,0 +1,19 @@
+using System;
+
+    class Data3
+    {
+        static void Main()
+        {
+           int[,] tabel = new int[3, 4];
+
+           for (int t = 0; t < 3; t++) 
+           {
+               for (int i = 0; i < 4; i++)
+               {
+                   tabel[t, i] = (t * 4) + i + 1;
+                   Console.Write(tabel[t, i] + " ");
+               }
+               Console.WriteLine();
+           }
+        }
+    }
---
+++ jagged.cs
@@ -0,0 +1,12 @@
+using System;
+
+    class Jagged
+    {
+        static void Main()
+        {
+          int[][] jagged = new int[3][];
+          jagged[0] = new int[4];
+          jagged[1] = new int[3];
+          jagged[2] = new int[5];
+        }
+    }
