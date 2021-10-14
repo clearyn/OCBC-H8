0bb6d70aec1e9160997d70eac9f7fc2767ab8b02
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-10-14 08:32:41 +0700

    sesi7

---
+++ SESI7/LoginSystem/LoginSystem/config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+//Message Box
+using System.Windows.Forms;
+//Add data function and classes
+using System.Data;
+
+public class Config
+{
+	string ConnectionString = "";
+	public MySqlConnection connection = null;
+	public string server = "127.0.0.1";
+	public string user = "root";
+	public string password = "manu";
+	DataSet ds;
+	DataTable dt;
+	public string Table = "user_info";
+	public string ConnectionType = "";
+	string RecordSource = "";
+
+	DataGridView tempdata;
+
+	public Config()
+	{
+
+
+	}
+}
