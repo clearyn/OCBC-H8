a792bff4f91893df229d816ea0afe552147b8973
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-10-17 14:05:12 +0700

    ApiDbContext

---
+++ SESI10/TodoAppWithJWT/Data/ApiDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAppWithJWT.Models;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+
+namespace TodoAppWithJWT.Data
+{
+    //dengan mewarisi dari IdentityDbContext pada DbContext, EntityFramework akan tahu bahwa kita menggunakan otentikasi dan itu akan menggunakan tabel default identity
+    public class ApiDbContext : IdentityDbContext
+    {
+        public virtual DbSet<ItemData> Items { get; set;}
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base (options)
+        {
+            
+        } 
+    }
+}
