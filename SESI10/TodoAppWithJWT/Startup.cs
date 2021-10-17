d3ce34c945104872c7d3babac0e71a573b78fae5
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-10-17 13:57:05 +0700

    initial commit

--- SESI10/TodoAppWithJWT/Startup.cs
+++ SESI10/TodoAppWithJWT/Startup.cs
@@ -4,19 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using TodoApp.Data;
-
-
-
+using TodoAppWithJWT.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Identity;
 
 namespace TodoApp
 {
@@ -31,11 +29,37 @@ namespace TodoApp
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {   
+            //JWT
+            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
+            //ASP.NET middleware & IOC Container
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(jwt => {
+                var key = Encoding.ASCII.GetBytes(Configuration["JwtConfig:Secret"]);
+
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience =false,
+                    ValidateLifetime = true,
+                    RequireExpirationTime = false
+                };
+            });
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApiDbContext>();
+            //SQLite
             services.AddDbContext<ApiDbContext>(options =>
             options.UseSqlite(
                 Configuration.GetConnectionString("DefaultConnection")
             ));
+            //Other
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -45,7 +69,8 @@ namespace TodoApp
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {   
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,6 +81,9 @@ namespace TodoApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //Autentikasi
+            app.UseAuthentications();
 
             app.UseAuthorization();
 
