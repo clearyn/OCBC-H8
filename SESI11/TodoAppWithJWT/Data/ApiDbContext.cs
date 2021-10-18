18de6f29ca8da6855c6a7adc84bc56c30e9bcd23
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-10-18 10:33:06 +0700

    sesi 11

---
+++ SESI11/TodoAppWithJWT/Data/ApiDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAppWithJWT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace TodoAppWithJWT.Data
+{
+    //dengan mewarisi dari IdentityDbContext pada DbContext, EntityFramework akan tahu bahwa kita menggunakan otentikasi dan itu akan menggunakan tabel default identity
+    public class ApiDbContext : IdentityDbContext
+    {
+        public virtual DbSet<ItemData> Items { get; set;}
+        public virtual DbSet<RefreshToken> RefreshTokens { get; set;}
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base (options)
+        {
+            
+        } 
+    }
+}
---
+++ SESI11/TodoAppWithJWT/Startup.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using TodoAppWithJWT.Data;
+using TodoAppWithJWT.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Identity;
+
+namespace TodoAppWithJWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {   
+            //JWT
+            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
+            //ASP.NET middleware & IOC Container
+            var key = Encoding.ASCII.GetBytes(Configuration["JwtConfig:Secret"]);
+
+            var tokenValidationParams = new TokenValidationParameters {
+                ValidateIssueSigningKey = true,
+                IssuerSigningKey = new SymetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAuidence = false,
+                ValidateLifetime = true,
+                RequestExpirationTime = false,
+                ClockSkey = TimeSpan.Zero
+            };
+
+            services.AddSingleton(tokenValidationParams);
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(jwt => {
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = tokenValidationParams;
+            });
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApiDbContext>();
+            //SQLite
+            services.AddDbContext<ApiDbContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("DefaultConnection")
+            ));
+            //Other
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApp", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {   
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApp v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //Autentikasi
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
